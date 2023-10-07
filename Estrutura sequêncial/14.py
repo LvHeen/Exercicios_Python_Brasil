ed98af4866739276fd356f6a13c8870d0f8d86b1
Helena <147088639+LvHeen@users.noreply.github.com>
2023-10-07 13:06:49 -0300

    Delete Estrutura sequêncial/14.py

--- Estrutura sequêncial/14.py
+++
@@ -1,8 +0,0 @@
-peso = float(input('Peso do peixe em kg: '))
-excesso = peso - 50
-multa = 4 * excesso
-
-if peso < 50:
-    print('O peixe está dentro dos limites.')
-else:
-    print(f'O peixe excedeu {excesso}kg do peso permitido. Sua multa será de R${multa:.2f}')
