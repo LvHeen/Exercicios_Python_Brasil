3cd9d5532e820b8f2e0058c48a6b580ab089d0b4
Helena <147088639+LvHeen@users.noreply.github.com>
2023-10-07 13:06:32 -0300

    Delete Estrutura sequêncial/11.py

--- Estrutura sequêncial/11.py
+++
@@ -1,7 +0,0 @@
-n1 = int(input('Digite um número inteiro: '))
-n2 = int(input('Outro número inteiro: '))
-n3 = int(input('Agora um número real: '))
-
-r1 = (2 * n1 ) * (n2 / 2)
-r2 = (3 * n1) + n3
-r3 = n3 ** (1 / 3)
