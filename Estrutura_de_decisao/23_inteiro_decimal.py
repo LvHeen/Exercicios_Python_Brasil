543853c4f3262f27a9701039b7cda8cb51c53262
Helena <147088639+LvHeen@users.noreply.github.com>
2023-10-16 13:38:25 -0300

    Add files via upload

---
+++ Estrutura_de_decisao/23_inteiro_decimal.py
@@ -0,0 +1,7 @@
+'''Faça um Programa que peça um número e informe se o número é inteiro ou decimal. Dica: utilize uma função de arredondamento.'''
+
+numero = 0
+if(round(numero) == True):
+    print(f'{int(numero)} é um número inteiro!')
+else:
+    print(f'{numero} é um número decimal!')
