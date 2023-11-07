d7051a9fa519acf8c0f27d98f3518f123946c8bc
Helena <147088639+LvHeen@users.noreply.github.com>
2023-11-06 22:15:45 -0300

    Update 22_impar_par.py

--- Estrutura_de_decisao/22_impar_par.py
+++ Estrutura_de_decisao/22_impar_par.py
@@ -1,4 +1,5 @@
-'''Faça um Programa que peça um número inteiro e determine se ele é par ou impar. Dica: utilize o operador módulo (resto da divisão).'''
+'''Faça um Programa que peça um número inteiro e determine se ele é par ou impar. 
+Dica: utilize o operador módulo (resto da divisão).'''
 
 n = int(input('Digite um número inteiro: '))
 if (n % 2) == 0:
