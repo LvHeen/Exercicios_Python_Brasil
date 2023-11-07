0287f9646e7f2da4badd578fd17d3d43228a54f8
Helena <147088639+LvHeen@users.noreply.github.com>
2023-11-06 22:08:50 -0300

    Update 12_peso_ideal.py

--- Estrutura_sequencial/12_peso_ideal.py
+++ Estrutura_sequencial/12_peso_ideal.py
@@ -1,4 +1,5 @@
-'''Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal, usando a seguinte fórmula: (72.7*altura) - 58'''
+'''Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal, 
+usando a seguinte fórmula: (72.7*altura) - 58'''
 
 altura = float(input('Qual sua altura? '))
 peso = (72.7 * altura) - 58
