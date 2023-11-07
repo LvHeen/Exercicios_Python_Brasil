431a310380685965a833b0f8f2829599a22f1e10
Helena <147088639+LvHeen@users.noreply.github.com>
2023-11-06 22:12:21 -0300

    Update 10_periodo.py

--- Estrutura_de_decisao/10_periodo.py
+++ Estrutura_de_decisao/10_periodo.py
@@ -1,4 +1,6 @@
-'''Faça um Programa que pergunte em que turno você estuda. Peça para digitar M-matutino ou V-Vespertino ou N- Noturno. Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso.'''
+'''Faça um Programa que pergunte em que turno você estuda. Peça para digitar 
+M-matutino ou V-Vespertino ou N- Noturno. Imprima a mensagem "Bom Dia!", 
+"Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso.'''
 
 periodo = input('Em que periodo você estuda? [M- matutino / V- verpetino / N- noturno]: ').upper()
 if periodo == 'M':
