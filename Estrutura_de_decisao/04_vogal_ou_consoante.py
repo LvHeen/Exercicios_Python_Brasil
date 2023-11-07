fba61987fcc17afaf652075584fff8c3fe1a3733
Helena <147088639+LvHeen@users.noreply.github.com>
2023-11-06 22:11:34 -0300

    Update 04_vogal_ou_consoante.py

--- Estrutura_de_decisao/04_vogal_ou_consoante.py
+++ Estrutura_de_decisao/04_vogal_ou_consoante.py
@@ -1,5 +1,4 @@
-'''
-Faça um Programa que verifique se uma letra digitada é vogal ou consoante.'''
+'''Faça um Programa que verifique se uma letra digitada é vogal ou consoante.'''
 
 letra = str(input('Insira uma letra: ')).upper()
 if letra == 'A' and 'E' and 'O' and 'O' and 'U':
