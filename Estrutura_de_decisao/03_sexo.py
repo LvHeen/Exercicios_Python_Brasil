884bf51a837a0b77860f567065e2e52704386fb1
Helena <147088639+LvHeen@users.noreply.github.com>
2023-11-06 22:11:18 -0300

    Update 03_sexo.py

--- Estrutura_de_decisao/03_sexo.py
+++ Estrutura_de_decisao/03_sexo.py
@@ -1,4 +1,5 @@
-'''Faça um Programa que verifique se uma letra digitada é "F" ou "M". Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.'''
+'''Faça um Programa que verifique se uma letra digitada é "F" ou "M". 
+Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.'''
 
 s = input('Informe seu sexo [M / F]: ').upper()
 if s == 'M':
