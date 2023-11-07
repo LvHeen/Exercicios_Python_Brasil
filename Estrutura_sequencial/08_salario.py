ea843fad4abc84e88ec420aae661e2223eb2e3ee
Helena <147088639+LvHeen@users.noreply.github.com>
2023-11-06 22:08:23 -0300

    Update 08_salario.py

--- Estrutura_sequencial/08_salario.py
+++ Estrutura_sequencial/08_salario.py
@@ -1,4 +1,5 @@
-'''Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês'''
+'''Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. 
+Calcule e mostre o total do seu salário no referido mês'''
 
 valor_hora = float(input('Quanto você ganha por hora? R$'))
 qtas_hora = int(input('Quantas horas você trablhou nesse mês? '))
