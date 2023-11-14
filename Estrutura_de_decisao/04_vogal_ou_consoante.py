60e755ca67a76b7ce7724ae33708c7d74fbf5db5
Helena <147088639+LvHeen@users.noreply.github.com>
2023-11-14 14:50:33 -0300

    Delete Estrutura_de_decisao directory

--- Estrutura_de_decisao/04_vogal_ou_consoante.py
+++
@@ -1,9 +0,0 @@
-'''Faça um Programa que verifique se uma letra digitada é vogal ou consoante.'''
-
-letra = str(input('Insira uma letra: ')).upper()
-if letra == 'A' and 'E' and 'O' and 'O' and 'U':
-    print(f'{letra} é vogal')
-else:
-    print(f'{letra} é consoante')
-
-'''Tentei usar o operador "or" invés de "and", mas não funcionaram e não entendi o porquê'''
