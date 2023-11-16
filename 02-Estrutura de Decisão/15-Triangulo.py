40ffe514f48037698553b50fed3853c274d31a59
Helena <147088639+LvHeen@users.noreply.github.com>
2023-11-16 06:00:57 -0300

    Add files via upload

---
+++ 02-Estrutura de Decisão/15-Triangulo.py
@@ -0,0 +1,21 @@
+'''Faça um Programa que peça os 3 lados de um triângulo. O programa deverá informar se os valores podem ser um triângulo. Indique, caso os lados formem um triângulo, se o mesmo é: equilátero, isósceles ou escaleno.
+Dicas:
+Três lados formam um triângulo quando a soma de quaisquer dois lados for maior que o terceiro;
+Triângulo Equilátero: três lados iguais;
+Triângulo Isósceles: quaisquer dois lados iguais;
+Triângulo Escaleno: três lados diferentes;'''
+
+r1 = float(input('Primeiro segmento: '))
+r2 = float(input('Segundo segmento: '))
+r3 = float(input('Terceiro segmento: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Os segmentos acima podem formar um triângulo.')
+else:
+    print('Os segmentos acima não podem formar um triângulo.')
+
+if r1 == r2 == r3:
+    print('Esse é um triângulo EQUILATERO.')
+elif r1 == r2 or r1 == r3 or r2 == r3:
+    print('Esse é um triângulo ISÓSELES.')
+else:
+    print('Esse é um triângulo ESCALENO.')
